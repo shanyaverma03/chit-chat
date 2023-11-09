e3dbfaa77ad10de623d2546e9618400905d27758
Shanya <shanyaverma03@gmail.com>
2023-11-09 23:40:00 +0530

    Create pages

--- public/src/App.js
+++ public/src/App.js
@@ -1,5 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Register from "./pages/Register";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Chat /> },
+  { path: "/register", element: <Register /> },
+  { path: "/login", element: <Login /> },
+]);
+
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
