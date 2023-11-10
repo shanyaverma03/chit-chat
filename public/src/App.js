19588a4fa2306e61e5b370f2393e86f8cfef2951
Shanya <shanyaverma03@gmail.com>
2023-11-10 16:53:29 +0530

    Add functionality to set avatar as profile pic

--- public/src/App.js
+++ public/src/App.js
@@ -1,12 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import Register from "./pages/Register";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
+import SetAvatar from "./pages/SetAvatar";
 
 const router = createBrowserRouter([
   { path: "/", element: <Chat /> },
   { path: "/register", element: <Register /> },
   { path: "/login", element: <Login /> },
+  { path: "/setAvatar", element: <SetAvatar /> },
 ]);
 
 function App() {
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,13 @@
-const { register } = require("../controllers/usersController");
-const { login } = require("../controllers/usersController");
-
 const router = require("express").Router();
+
+const {
+  register,
+  setAvatar,
+  login,
+} = require("../controllers/usersController");
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/setAvatar/:id", setAvatar);
 
 module.exports = router;
